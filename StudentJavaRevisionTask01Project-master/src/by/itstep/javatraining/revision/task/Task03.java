bc1948abfb28625d64a0dff4f7172b1b7ebdb4fa
User <User@User-PC>
2024-04-02 10:32:01 +0300

    Resolve task 3

--- StudentJavaRevisionTask01Project-master/src/by/itstep/javatraining/revision/task/Task03.java
+++ StudentJavaRevisionTask01Project-master/src/by/itstep/javatraining/revision/task/Task03.java
@@ -26,6 +26,6 @@ package by.itstep.javatraining.revision.task;
 
 public class Task03 {
     public static int start(int number) {
-        return 0;
+        return Math.abs(number) % 100 / 10;
     }
 }
